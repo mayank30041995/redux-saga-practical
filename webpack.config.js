e84b586ac50e5ec883ccac9840d727289e60b075
Mayank Sharma <mayankece155@gmail.com>
2022-02-10 23:56:58 +0530

    setup final

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: path.join(__dirname, "src", "index.js"),
   output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist")
+    path:path.resolve(__dirname, "dist"),
   },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 9000
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
   },
-  mode: "development"
-};
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+}
